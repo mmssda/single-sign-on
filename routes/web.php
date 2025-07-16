69c25452bad4f83d1e7642256387fbc5868dfb95
teguh <teguh.septiyan111@gmail.com>
2025-07-16 10:01:36 +0700

    create route web group owner for login and dashboard startup

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,21 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::middleware(['web'])
+        ->prefix('owner')
+        ->group(function () {
+
+     Route::post('login', 'Owner\LoginController@login');
+
+    //  Route::get('login', function(){
+    //    return response()->json(['_token' => csrf_token()]);
+    // });
+
+    Route::middleware(['auth', 'owner'])->group(function () {
+        Route::get('dashboard', function () {
+            return 'Halaman Dashboard Owner';
+        })->name('owner.dashboard');
+    });
+});
